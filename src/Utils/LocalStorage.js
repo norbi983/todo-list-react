1eba377093e5f9fdc8b234b35f82a9006f57342c
Norbert Mszyca <norbi983@interia.pl>
2023-02-07 14:27:31 +0100

    delete

--- src/Utils/LocalStorage.js
+++
@@ -1,8 +0,0 @@
-const toLocalStorage = (tasks) => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-};
-
-const fromLocalStorage = () =>
-JSON.parse(localStorage.getItem("tasks"))
-
-export { toLocalStorage, fromLocalStorage };
